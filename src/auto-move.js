72f9c106034e2516c072a97926f76a4b7936f1c4
Chelsea Thompto <chelsea.l.thompto@gmail.com>
2026-02-22 16:58:33 -0500

    hide framerate hud

--- src/auto-move.js
+++ src/auto-move.js
@@ -169,8 +169,8 @@ var previousTime = 0;
 // stats
 
 var stats = new Stats();
-stats.showPanel(0); // 0: fps, 1: ms, 2: mb, 3+: custom
-document.body.appendChild(stats.dom);
+//stats.showPanel(0); // 0: fps, 1: ms, 2: mb, 3+: custom
+//document.body.appendChild(stats.dom);
 
 // scene start
 
